d34ae0451113f7223f299a19d1c745660c60664c
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-05-19 14:59:17 +0630

    Add Pagination

--- resources/views/components/listing-section.blade.php
+++ resources/views/components/listing-section.blade.php
@@ -3,7 +3,11 @@
                 class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
             >
                 <!-- Item 1 -->
+                @unless (count($listings) == 0)
                 @foreach ($listings as $listing)
                     <x-card :listing="$listing"/>
                 @endforeach
+                @else
+                    <p>No Lists Found.</p>
+                @endunless
             </div>
